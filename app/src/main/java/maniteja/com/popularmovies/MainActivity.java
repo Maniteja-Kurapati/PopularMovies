b6b8c613862e70d3a56e05e71dc9a2501b67771a
maniteja <maniteja.kurapati@gmail.com>
2016-09-04 00:13:13 -0500

    Initial Commit

---
+++ app/src/main/java/maniteja/com/popularmovies/MainActivity.java
@@ -0,0 +1,13 @@
+package maniteja.com.popularmovies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
