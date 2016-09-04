bf3d19e622ad1248f675a39e103a4f38591220b4
maniteja <maniteja.kurapati@gmail.com>
2016-09-04 00:56:58 -0500

    Added MovieList Fragmnet with grid adapter

--- app/src/main/java/maniteja/com/popularmovies/MainActivity.java
+++ app/src/main/java/maniteja/com/popularmovies/MainActivity.java
@@ -1,13 +1,29 @@
 package maniteja.com.popularmovies;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import maniteja.com.popularmovies.dummy.DummyContent;
+
+public class MainActivity extends AppCompatActivity implements MovieListFragment.OnListFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MovieListFragment movieListFragment=new MovieListFragment();
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction=manager.beginTransaction();
+        transaction.add(R.id.movieFragment_container,movieListFragment);
+        transaction.commit();
+
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
     }
 }
